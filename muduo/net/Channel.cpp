caea76bad64c2bc294a3e7dc24be11766f0e84fa
Penguin-SAMA <496107146@qq.com>
2025-03-13 23:08:17 +0800

    update Channel module

--- muduo/net/Channel.cpp
+++ muduo/net/Channel.cpp
@@ -3,6 +3,8 @@
 namespace muduo {
 namespace net {
 
+// 只对 fd 做简单的引用，不负责关闭 fd
+// 所以构造函数不负责打开 fd，析构函数不负责关闭 fd
 Channel::Channel(int fd)
     : fd_(fd)
     , events_(0)
@@ -13,21 +15,30 @@ Channel::~Channel() {
 }
 
 void Channel::handleEvent() {
-    if ((revents_ & 0x20) && closeCallback_) {
-        closeCallback_();
-    } else if ((revents_ & 0x08) && errorCallback_) {
-        errorCallback_();
-    } else {
-        if ((revents_ & 0x01) && readCallback_) {
-            readCallback_();
-        }
-        if ((revents_ & 0x04) && writeCallback_) {
-            writeCallback_();
-        }
-    }
+    // 为了安全，先做一个shared_ptr的延长生命周期
+    // 防止在回调函数中将自己析构
+    handleEventWithGuard();
 }
 
 void Channel::handleEventWithGuard() {
+    // 根据revents_ 的值，调用对应的回调函数
+    // 判断条件根据 poll 的返回值定义
+    if ((revents_ & 0x20) && closeCallback_) {
+        // 比如 EPOLLRDHUP 事件
+        closeCallback_();
+    } else if ((revents_ & 0x08) && errorCallback_) {
+        // 比如 EPOLLERR 事件
+        errorCallback_();
+    } else {
+        if ((revents_ & 0x01) && readCallback_) {
+            // 比如 EPOLLIN 事件
+            readCallback_();
+        }
+        if ((revents_ & 0x04) && writeCallback_) {
+            // 比如 EPOLLOUT 事件
+            writeCallback_();
+        }
+    }
 }
 
 }
--- muduo/net/Channel.h
+++ muduo/net/Channel.h
@@ -6,37 +6,54 @@
 namespace muduo {
 namespace net {
 
+// Channel 负责封装一个文件描述符以及其关心的IO事件
+// 并在事件就绪时调用对应的回调函数
+// 关键点：
+// Channel并不拥有这个文件描述符，也不会在析构时关闭这个文件描述符
+// fd的生命周期由Acceptor/Connector/EventLoop等类管理
+// Channel只负责监听fd上的IO事件，并在事件就绪时调用对应的回调函数
 class Channel : public std::enable_shared_from_this<Channel>
 {
 public:
     using EventCallback = std::function<void()>;
 
+    // @param fd 文件描述符
     Channel(int fd);
     ~Channel();
 
+    // 设置可读事件回调函数
     void setReadCallback(const EventCallback& cb) { readCallback_ = cb; }
+    // 设置可写事件回调函数
     void setWriteCallback(const EventCallback& cb) { writeCallback_ = cb; }
+    // 设置关闭事件回调函数
     void setCloseCallback(const EventCallback& cb) { closeCallback_ = cb; }
+    // 设置错误事件回调函数
     void setErrorCallback(const EventCallback& cb) { errorCallback_ = cb; }
 
+    // 当poller发现fd上的事件时，调用该函数
     void handleEvent();
 
+    // 设置fd上关心的事件
     void setEvents(int events) { events_ = events; }
     int events() const { return events_; }
 
+    // 设置fd上的事件
     void setRevents(int revents) { revents_ = revents; }
     int revents() const { return revents_; }
 
+    // 返回fd
     int fd() const { return fd_; }
 
 private:
+    // 用于处理事件的函数，内部会调用handleEvent
     void handleEventWithGuard();
 
 private:
-    const int fd_;
-    int events_;
-    int revents_;
+    const int fd_; // 文件描述符
+    int events_;   // 关心的事件
+    int revents_;  // 就绪的事件
 
+    // 四种事件的回调函数
     EventCallback readCallback_;
     EventCallback writeCallback_;
     EventCallback closeCallback_;
